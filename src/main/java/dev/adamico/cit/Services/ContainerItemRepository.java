83fef12d5d521bfb240dd4a7cc0db6d51c69ad9b
Alan DAmico <adamico1@uncc.edu>
2024-05-10 13:21:49 -0400

    Added repository annotation

--- src/main/java/dev/adamico/cit/Services/ContainerItemRepository.java
+++ src/main/java/dev/adamico/cit/Services/ContainerItemRepository.java
@@ -2,6 +2,9 @@ package dev.adamico.cit.Services;
 
 import dev.adamico.cit.Models.ContainerItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContainerItemRepository extends JpaRepository<ContainerItem, Long> {
 }
+
