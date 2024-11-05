0f6517418d99dd706cdab7bb047de4ad955c6622
Alan DAmico <adamico1@uncc.edu>
2024-11-05 11:36:48 -0500

    Fixed issue with routing to child paths

---
+++ src/main/java/dev/adamico/cit/Redirect.java
@@ -0,0 +1,13 @@
+package dev.adamico.cit;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class Redirect {
+    @RequestMapping("/{path:[^\\.]+}/**")
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
