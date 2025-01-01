ec0e6f9825091b0066cd79b03f199fd427445b2f
Alan DAmico <adamico1@uncc.edu>
2025-01-01 14:09:49 -0500

    Set up default values for delimiters

---
+++ src/main/resources/db/migration/V19__DefaultSettings.sql
@@ -0,0 +1,2 @@
+INSERT INTO settings_table (id, key, value) VALUES (nextval('settings_table_seq'), 'containerDelimiter', '|');
+INSERT INTO settings_table (id, key, value) VALUES (nextval('settings_table_seq'), 'itemDelimiter', '|');
