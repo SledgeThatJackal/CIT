24c4dda3b4a9a92ad0c65577b109a3efe40c6584
Alan DAmico <adamico1@uncc.edu>
2025-01-08 18:57:49 -0500

    Added the testing button for bulk creating

--- src/main/frontend/src/app/pages/home/HomePage.tsx
+++ src/main/frontend/src/app/pages/home/HomePage.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import DatabaseExportButton from "@components/general/DatabaseExportButton";
 import { NavLink } from "react-router-dom";
+import BulkCreateButton from "@components/bulk_create/BulkCreateButton";
 
 function Home() {
   return (
     <>
       <p>Home</p>
       <DatabaseExportButton />
+      <div className="mt-2">
+        <BulkCreateButton />
+      </div>
     </>
   );
 }
