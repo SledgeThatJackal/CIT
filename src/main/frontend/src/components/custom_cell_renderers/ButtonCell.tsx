ebeaa8b525da2f59bdab8c89977a635c5bfb6cf8
Alan DAmico <adamico1@uncc.edu>
2025-01-09 14:09:09 -0500

    Added disabled prop

--- src/main/frontend/src/components/custom_cell_renderers/ButtonCell.tsx
+++ src/main/frontend/src/components/custom_cell_renderers/ButtonCell.tsx
@@ -4,6 +4,7 @@ import { Button, Container } from "react-bootstrap";
 type ButtonCellProps = {
   title: string;
   color: string;
+  isDisabled?: boolean;
   handleClick: () => void;
   children: any;
 };
@@ -11,12 +12,17 @@ type ButtonCellProps = {
 const ButtonCell = ({
   title,
   color,
+  isDisabled,
   handleClick,
   children,
 }: ButtonCellProps) => {
   return (
     <Container fluid className="p-0 m-0 w-100 d-flex justify-content-center">
-      <Button onClick={handleClick} variant={color} title={title}>
+      <Button
+        onClick={handleClick}
+        variant={color}
+        title={title}
+        disabled={isDisabled}>
         {children}
       </Button>
     </Container>
