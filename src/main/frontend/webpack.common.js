6f26d74099c2e32aa105ff45e721ee80ca157f94
Alan DAmico <adamico1@uncc.edu>
2025-01-14 16:39:39 -0500

    Fixed babel preset for React

--- src/main/frontend/webpack.common.js
+++ src/main/frontend/webpack.common.js
@@ -43,7 +43,15 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: [
+              "@babel/preset-env",
+              [
+                "@babel/preset-react",
+                {
+                  runtime: "automatic",
+                },
+              ],
+            ],
           },
         },
       },
