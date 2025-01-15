ef1d6f8fc284de2b586c6422c17dadb830de5e8a
Alan DAmico <adamico1@uncc.edu>
2025-01-15 11:42:23 -0500

    Removed zip upload input

--- src/main/frontend/src/app/pages/home/HomePage.tsx
+++ src/main/frontend/src/app/pages/home/HomePage.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 import DatabaseExportButton from "@components/general/DatabaseExportButton";
-import BulkCreateButton from "@container/components/zip_create/BulkCreateButton";
 
 function Home() {
   return (
     <>
       <p>Home</p>
       <DatabaseExportButton />
-      <div className="mt-2">
-        <BulkCreateButton />
-      </div>
     </>
   );
 }
