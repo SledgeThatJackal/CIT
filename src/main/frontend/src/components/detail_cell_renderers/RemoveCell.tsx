b5b2e98697625d273fbac02d2819fdf15d128324
Alan DAmico <adamico1@uncc.edu>
2025-01-17 13:40:20 -0500

    Altered functionality, so it will open a modal instead of instantly deleting the row

--- src/main/frontend/src/components/detail_cell_renderers/RemoveCell.tsx
+++ src/main/frontend/src/components/detail_cell_renderers/RemoveCell.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { useModalState } from "@hooks/state/useModalState";
 import { useDeleteLink } from "@services/mutations";
 import { CellContext } from "@tanstack/react-table";
 import React from "react";
@@ -9,9 +10,15 @@ const RemoveCell = <T, S extends number>({
   table,
 }: CellContext<T, S>) => {
   const deleteLinkMutation = useDeleteLink();
+  const { openMessageModal } = useModalState();
 
   const handleClick = () => {
-    deleteLinkMutation.mutate(getValue());
+    openMessageModal(
+      "Delete Link",
+      "Delete",
+      () => deleteLinkMutation.mutate(getValue()),
+      "Are you sure you want to remove this link?",
+    );
   };
 
   return (
