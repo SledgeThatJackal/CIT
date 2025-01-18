ca67a3cff4635f39f1d7b84ff62d94efe278ade7
Alan DAmico <adamico1@uncc.edu>
2025-01-18 15:39:22 -0500

    Set up method to get all itemTypes and their count

--- src/main/java/dev/adamico/cit/Repositories/ItemRepository.java
+++ src/main/java/dev/adamico/cit/Repositories/ItemRepository.java
@@ -3,8 +3,14 @@ package dev.adamico.cit.Repositories;
 import dev.adamico.cit.Models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
+
+    @Query("SELECT i.itemType, COUNT(i) FROM Item i GROUP BY i.itemType")
+    List<Object[]> findItemTypesAndCounts();
 }
