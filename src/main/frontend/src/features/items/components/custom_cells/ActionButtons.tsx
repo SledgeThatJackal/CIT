40bbe224568c7ff2ea3105c1aabd4b9e4736f25f
Alan DAmico <adamico1@uncc.edu>
2025-01-18 13:33:51 -0500

    Added the delete button

--- src/main/frontend/src/features/items/components/custom_cells/ActionButtons.tsx
+++ src/main/frontend/src/features/items/components/custom_cells/ActionButtons.tsx
@@ -1,22 +1,24 @@
 import { Row } from "@tanstack/react-table";
 import React from "react";
-import { Button, InputGroup } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { Item } from "../../schemas/Item";
 import { useActionState } from "../../hooks/useActionState";
 import { useCanvasState } from "@hooks/state/useCanvasState";
 import LinkBox from "../LinkBox";
 import CreateBox from "../CreateBox";
 
+import "@styles/ActionsCell.css";
+import DeleteCell from "@components/write/DeleteCell";
+import DetailCell from "@components/detail_cell_renderers/DetailCell";
+
 type ActionButtonsProps = {
-  table: any;
+  table: unknown;
   row: Row<Item>;
 };
 
 const ActionButtons = ({ row }: ActionButtonsProps) => {
-  const { callerId, updateItemAction, clearAction } = useActionState();
-  const { openCanvas, closeCanvas } = useCanvasState();
-
-  const isActive = callerId === row.getValue("id");
+  const { updateItemAction } = useActionState();
+  const { openCanvas } = useCanvasState();
 
   const handleOpen = () => {
     openCanvas(LinkBox, "bottom", `Editing: ${row.getValue("name")}`);
@@ -30,13 +32,9 @@ const ActionButtons = ({ row }: ActionButtonsProps) => {
     updateItemAction(row.original);
   };
 
-  const handleClose = () => {
-    closeCanvas();
-    clearAction();
-  };
-
   return (
-    <InputGroup className="p-0 m-0 w-100 d-flex justify-content-center">
+    <Container className="actions-container">
+      <DetailCell row={row} />
       <Button
         size="sm"
         {...{ onClick: row.getToggleExpandedHandler() }}
@@ -51,20 +49,15 @@ const ActionButtons = ({ row }: ActionButtonsProps) => {
         title="Duplicate">
         <i className="bi bi-clipboard" />
       </Button>
-      {isActive ? (
-        <Button variant="info" size="sm" onClick={handleClose} title="Close">
-          ●
-        </Button>
-      ) : (
-        <Button
-          variant="success"
-          size="sm"
-          onClick={handleOpen}
-          title="Edit Containers">
-          +
-        </Button>
-      )}
-    </InputGroup>
+      <Button
+        variant="warning"
+        size="sm"
+        onClick={handleOpen}
+        title="Edit Containers">
+        <i className="bi bi-pencil" />
+      </Button>
+      <DeleteCell id={row.getValue("id")} />
+    </Container>
   );
 };
 
