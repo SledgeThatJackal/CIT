b5c46a33cee77f1b1ea45803798694b4abeb7d57
Alan DAmico <adamico1@uncc.edu>
2025-01-18 15:39:57 -0500

    Added the statics tables to the home page

--- src/main/frontend/src/app/pages/home/HomePage.tsx
+++ src/main/frontend/src/app/pages/home/HomePage.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import ContainerTable from "@home/components/ContainerTable";
+import ItemTable from "@home/components/ItemTable";
+import React, { createContext } from "react";
+import { Container } from "react-bootstrap";
+import { HomeType } from "src/features/home/schemas/Home";
 
-import DatabaseExportButton from "@components/general/DatabaseExportButton";
+import { useHome } from "src/features/home/services/query";
+
+import "@home/styles/HomePage.css";
+
+export const HomeContext = createContext<HomeType | undefined>(undefined);
 
 function Home() {
+  const homeQuery = useHome().data;
+
+  if (!homeQuery) return <div>No data found</div>;
+
   return (
-    <>
-      <p>Home</p>
-      <DatabaseExportButton />
-    </>
+    <Container fluid className="home-container">
+      <HomeContext.Provider value={homeQuery}>
+        <ContainerTable />
+        <ItemTable />
+      </HomeContext.Provider>
+    </Container>
   );
 }
 
