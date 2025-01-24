93a87c1b112d1583a01571a4a6835eb782fea79e
Alan DAmico <adamico1@uncc.edu>
2025-01-24 13:05:04 -0500

    Added styling to UI components

--- src/main/frontend/src/features/images/components/FindImage.tsx
+++ src/main/frontend/src/features/images/components/FindImage.tsx
@@ -4,6 +4,7 @@ import {
   FloatingLabel,
   Form,
   InputGroup,
+  Stack,
 } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import { useImageFinds, useImageFindsTotal } from "../services/query";
@@ -40,30 +41,56 @@ const FindImage = () => {
   useEffect(() => handleClick(), []);
 
   return (
-    <Container fluid className="mt-2 w-75 find-container bg-dark">
-      <Container className="text-light p-0 m-0 pb-2">{`Displaying ${imageFindsQuery?.length || 0} out of ${imageFindsTotal.data || 0} Images`}</Container>
-      <Container fluid className="p-0">
-        <InputGroup>
+    <Container fluid className="mt-2 w-75 find-container bg-dark shadow">
+      <Stack direction="horizontal" className="text-light p-0 m-0 pb-2">
+        <span>
+          <strong>Image Search</strong>
+        </span>
+        <span className="ms-auto">
+          <strong>{`Displaying ${imageFindsQuery?.length || 0} out of ${imageFindsTotal.data || 0} Images`}</strong>
+        </span>
+      </Stack>
+      <Container fluid className="p-0 shadow">
+        <InputGroup className="shadow">
           <FloatingLabel
             label={"Container Name"}
-            controlId="floatingContainerName">
+            controlId="floatingContainerName"
+            className="image-floating">
             <Form.Control
               type="text"
               name="containerName"
               onChange={handleChange}
+              className="image-input"
+              autoComplete="off"
             />
           </FloatingLabel>
-          <FloatingLabel label={"Barcode ID"} controlId="floatingScannerId">
+          <FloatingLabel
+            label={"Barcode ID"}
+            controlId="floatingScannerId"
+            className="image-floating">
             <Form.Control
               type="text"
               name="scannerId"
               onChange={handleChange}
+              className="image-input"
+              autoComplete="off"
             />
           </FloatingLabel>
-          <FloatingLabel label={"Item Name"} controlId="floatingItemName">
-            <Form.Control type="text" name="itemName" onChange={handleChange} />
+          <FloatingLabel
+            label={"Item Name"}
+            controlId="floatingItemName"
+            className="image-floating">
+            <Form.Control
+              type="text"
+              name="itemName"
+              onChange={handleChange}
+              className="image-input"
+              autoComplete="off"
+            />
           </FloatingLabel>
-          <Button onClick={handleClick}>Search</Button>
+          <Button onClick={handleClick} className="image-search">
+            Search
+          </Button>
         </InputGroup>
       </Container>
       <Container fluid className="image-container">
@@ -72,8 +99,8 @@ const FindImage = () => {
           imageFindsQuery?.map((image, index) => (
             <div
               key={`imageFind-${image.scannerId}-${index}`}
-              className="image-div text-center"
-              title={image.scannerId}>
+              className="image-div text-center shadow"
+              title={image.fileName}>
               <label>{image.scannerId}</label>
               <NavLink to={`/container/${image.scannerId}`}>
                 <img
